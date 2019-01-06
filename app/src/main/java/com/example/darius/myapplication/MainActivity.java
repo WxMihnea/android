457864a17c5ba803f6973cc00eaf811873a4e329
Darius Galanton <fortech.galanton@extaccount.com>
2019-01-06 21:54:16 +0200

    finished project

--- app/src/main/java/com/example/darius/myapplication/MainActivity.java
+++ app/src/main/java/com/example/darius/myapplication/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         initLoginView();
     }
 
